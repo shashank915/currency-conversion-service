a4b1c46bd6466d8bc8b99a17e6c504b4f6e30069
Shashank <shashank@frostinteractive.com>
2018-09-05 18:35:57 +0530

    updated

--- src/main/java/com/example/currencyconversionservice/controller/ForexControllerProxy.java
+++ src/main/java/com/example/currencyconversionservice/controller/ForexControllerProxy.java
@@ -1,12 +1,13 @@
 package com.example.currencyconversionservice.controller;
 
 import com.example.currencyconversionservice.domain.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "forex-service")
-
+@RibbonClient(name = "forex-service")
 public interface ForexControllerProxy {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
